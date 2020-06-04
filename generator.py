d921157c0659fe9396beaf40c1ac61e45c0c9493
cavebutter <jay@realpm.net>
2020-06-04 14:59:51 -0700

    committed selectable comumns changes

--- generator.py
+++ generator.py
@@ -35,6 +35,9 @@ with open('data/colors.csv', newline='') as f:
     reader = csv.reader(f, delimiter = ",")
     colors = list(reader)
     
+# Flatten the lists of lists
+dogs = dg.flatten_lol(dogs)  
+colors = dg.flatten_lol(colors)
 
 del foo
 
@@ -50,65 +53,83 @@ num_list_4 = [random.randint(1, 10) for x in range(500)]
 
 
 # Get information about the table
+col_list = []
+col_headers = []
+
 table_name = input('What is the name of your table? ')
 include_index = input('Do you want your table to be indexed? (y/n) ')
-num_str_columns = int(input('How many text columns in your table? (Max 4) ')) 
-num_num_columns = int(input('How many numeric columns in your table? (Max 4)'))
 row_count = int(input('How many rows of data in your table? '))
-
-# Flatten the lists of lists
-dogs = dg.flatten_lol(dogs)  
-colors = dg.flatten_lol(colors)
-
-# Generate the lists based on row_count
 if include_index == "y":
     index = dg.add_index(row_count)
-else:
-    index = False
-animals_list = dg.random_index(row_count,animals)
-flowers_list = dg.random_index(row_count,flowers)
-trees_list = dg.random_index(row_count,trees)
-countries_list = dg.random_index(row_count,countries)
-colors_list = dg.random_index(row_count,colors)
-dogs_list = dg.random_index(row_count,dogs)
-num_1 = dg.generate_num_col(row_count,num_list_1)
-num_2 = dg.generate_num_col(row_count,num_list_2)
-num_3 = dg.generate_num_col(row_count,num_list_3)
-num_4 = dg.generate_num_col(row_count,num_list_4)
+    col_list.append(index)
+    col_headers.append('ID')
+# Routine to determine number and type of columns
+num_columns = int(input('How many columns in your table? (Max 8) '))
+i = 0
+while i < num_columns:
+    i += 1
+    item = input("""What type of data would you like?
+(1)  Animals
+(2)  Flowers
+(3)  Trees
+(4)  Countries
+(5)  Colors
+(6)  Dogs
+(7)  Numbers in range 1-500
+(8)  Numbers in range 501-10000
+(9)  Numbers in range 10000-900000
+(10) Numbers in range 1-10
+Enter the number of your choice: """)
+    header = input("What is the name of this column? ")
+    col_headers.append(header)
+    if item == '1':
+        animals_list = dg.random_index(row_count, animals)
+        col_list.append(animals_list)
+    elif item == '2':
+        flowers_list = dg.random_index(row_count, flowers)
+        col_list.append(flowers_list)
+    elif item == '3':
+        trees_list = dg.random_index(row_count, trees)
+        col_list.append(trees_list)
+    elif item == '4':
+        countries_list = dg.random_index(row_count, countries)
+        col_list.append(countries_list)
+    elif item == '5':
+        colors_list = dg.random_index(row_count, colors)
+        col_list.append(colors_list)
+    elif item == '6':
+        dogs_list = dg.random_index(row_count, dogs)
+        col_list.append(dogs_list)
+    elif item == '7':
+        num_1 = dg.generate_num_col(row_count,num_list_1)
+        col_list.append(num_1)
+    elif item == '8':
+        num_2 = dg.generate_num_col(row_count,num_list_2)
+        col_list.append(num_2)
+    elif item == '9':
+        num_3 = dg.generate_num_col(row_count,num_list_3)
+        col_list.append(num_3)
+    elif item == '10':
+        num_4 = dg.generate_num_col(row_count,num_list_4)
+        col_list.append(num_4)
+    else:
+        print('Make a better choice')
+    
 
-# Field length calcs for text output formatting
-animal_length = dg.longest_value(animals_list)
-flower_length = dg.longest_value(flowers_list)
-tree_length = dg.longest_value(trees_list)
-country_length = dg.longest_value(countries_list)
-color_length = dg.longest_value(colors_list)
-dog_length = dg.longest_value(dogs_list)
-num_1_length = dg.longest_value(num_list_1)
-num_2_length = dg.longest_value(num_list_2)
-num_3_length = dg.longest_value(num_list_3)
-num_4_length = dg.longest_value(num_list_4)
+
+
 
 # Commbined list of generated lists
 
-# If no index
-if index == False:
-    master = list(zip(animals_list,colors_list,flowers_list,trees_list,dogs_list,countries_list,num_1,num_2,num_3,num_4))
+
+master = list(zip(*col_list))
 
 # Output as csv
-    with open('output\\' + table_name + ".csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(['animal', 'color', 'flower', 'tree', 'dog', 'country', 'num1', 'num2', 'num3', 'num4'])
-        writer.writerows(master)
+with open('output\\' + table_name + ".csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(col_headers)
+    writer.writerows(master)
 
-# If yes index
-else:
-    master = list(zip(index,animals_list,colors_list,flowers_list,trees_list,dogs_list,countries_list,num_1,num_2,num_3,num_4))
-
-# Output as csv
-    with open('output\\' + table_name + ".csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(['ID','animal', 'color', 'flower', 'tree', 'dog', 'country', 'num1', 'num2', 'num3', 'num4'])
-        writer.writerows(master)
 
 # Completion Message
 print("Your sample data CSV file is ready")
